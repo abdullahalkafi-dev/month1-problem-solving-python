0411669a65f3a965f2bf64515161bcab0750f31b
abdullahalkafi-dev <abdullahalkafi742@gmail.com>
2026-03-23 12:47:40 +0600

    Enhance fizzbuzz_variant function and update tests; add support for 'Pop' output and correct test assertions

--- day01/fizzbuzz_variant.py
+++ day01/fizzbuzz_variant.py
@@ -7,10 +7,14 @@ def fizzbuzz_variant(n: int) -> str:
         result += "Fizz"
     if n % 5 == 0:
         result += "Buzz"
+    if n%7 ==0: 
+        result += "Pop"
     return result if result else str(n)
 
 if __name__ == "__main__":
     print(fizzbuzz_variant(3))
     print(fizzbuzz_variant(5))
+    print(fizzbuzz_variant(14))
     print(fizzbuzz_variant(15))
+    print(fizzbuzz_variant(105))
     print(fizzbuzz_variant(17))
--- day01/test_day01.py
+++ day01/test_day01.py
@@ -2,15 +2,15 @@ from fizzbuzz_variant import fizzbuzz_variant
 
 
 def test_fizzbuzz():
-    assert fizzbuzz_variant(3)=="Fizz"
-    assert fizzbuzz_variant(5)=="Buzz"
-    assert fizzbuzz_variant(15)=="FizzBuzz"
-    assert fizzbuzz_variant(3)=="Fizz"
-    assert fizzbuzz_variant(3)=="Fizz"
-    assert fizzbuzz_variant(3)=="Fizz"
-   # Edge Cases
+    assert fizzbuzz_variant(3) == "Fizz"
+    assert fizzbuzz_variant(5) == "Buzz"
+    assert fizzbuzz_variant(14) == "Pop"
+    assert fizzbuzz_variant(15) == "FizzBuzz"
+    assert fizzbuzz_variant(105) == "FizzBuzzPop"
+    # Edge Cases
     assert fizzbuzz_variant(1) == "1"
-    assert fizzbuzz_variant(0) == "FizzBuzz" 
+    assert fizzbuzz_variant(0) == "FizzBuzzPop"
     print("✅ All FizzBuzz tests passed!")
+
 
 test_fizzbuzz()
