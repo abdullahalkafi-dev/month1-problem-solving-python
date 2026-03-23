b0f6756afd096135196f9cd0038f72894397ac04
abdullahalkafi-dev <abdullahalkafi742@gmail.com>
2026-03-23 11:54:37 +0600

    Add print_even function to print even numbers up to a specified limit

---
+++ day01/warmup/even_numbers.py
@@ -0,0 +1,9 @@
+def print_even(limit:int)->None :
+    """It takes an int as limit And print the evens from 0  to that limit."""
+    if not isinstance(limit, int):
+        raise TypeError("Only integer is allowed as valid limit")    
+    for n in range(0,limit+1,2):
+        print(n)
+
+print_even(10)
+    
