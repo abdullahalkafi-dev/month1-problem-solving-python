366b8291219916d15b973babde7e43daeeb4d975
abdullahalkafi-dev <abdullahalkafi742@gmail.com>
2026-04-02 17:11:41 +0600

    Refactor BankAccount class constructor and error handling for input validation

--- day05/bank_account.py
+++ day05/bank_account.py
@@ -1,13 +1,21 @@
 class BankAccount:
     def __init__(self, account_number, owner_name, balance):
+        if not isinstance(account_number, str):
+            raise TypeError("account_number data type not allowed")
+        if not isinstance(owner_name, str):
+            raise TypeError("owner_name data type not allowed")
+        if not isinstance(balance, (int, float)):
+            raise TypeError("balance data type not allowed")
+        if balance < 0:
+            raise ValueError("balance must be greater than or equal to 0")
         self.account_number = account_number
         self.owner_name = owner_name
-        self._balance = balance
-        print("object created, balance={self._balance}")
+        self._balance = float(balance)
+        print(f"object created, balance={self._balance}")
 
     def deposit(self, amount: float) -> float:
         if not isinstance(amount, (int, float)):
-            raise typeError("amount data type not allowed")
+            raise TypeError("amount data type not allowed")
         if amount < 0:
             raise ValueError("amount must be grater than 0")
         self._balance = self._balance + amount
@@ -15,7 +23,7 @@ class BankAccount:
 
     def withdraw(self, amount: float) -> float:
         if not isinstance(amount, (int, float)):
-            raise typeError("amount data type not allowed")
+            raise TypeError("amount data type not allowed")
         if amount < 0:
             raise ValueError("amount must be grater than 0")
         if self._balance < amount:
