34e0e7bc4c758701680ae953dd093f079b844c31
abdullahalkafi-dev <abdullahalkafi742@gmail.com>
2026-04-06 02:05:28 +0600

    Add move_zeroes function: Implement a brute-force approach to move all zeroes in a list to the end while maintaining the order of non-zero elements

---
+++ day07/move_zeroes.py
@@ -0,0 +1,16 @@
+def move_zeroes(nums: list[int]) -> None:
+    """Brute-force approach"""
+    new_list = list()
+    count_zero = 0
+    for i in nums:
+        if i == 0:
+            count_zero += 1
+        else:
+            new_list.append(i)
+    for i in range(count_zero):
+        new_list.append(0)
+    nums[:] = new_list
+
+
+if __name__ == "__main__":
+    print(move_zeroes([0, 1, 2, 0, 0, 4, 5, 0]))
